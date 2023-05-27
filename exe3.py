a49fcdaa7087674c484c8a82e8539fb828fb97e4
Ednagit <ednadrizzle@gmail.com>
2023-05-26 19:48:18 -0500

    Modified exe3

--- exe3.py
+++ exe3.py
@@ -1,3 +1,12 @@
-#Write a program that asks the user to enter two numbers and compares them using the following comparison operators: 
-#>`, `<`, `>=`, `<=`, `==`, and `!=`. 
-#The program should print out the result of each comparison.
+# Write a program that asks the user to enter two numbers and compares them using the following comparison operators: 
+# >`, `<`, `>=`, `<=`, `==`, and `!=`. 
+# The program should print out the result of each comparison.
+num1=int(input('Enter the first number:'))
+num2=int(input('Enter the second number:'))
+
+print(num1>num2) # performs the greater than operation
+print(num1<num2) # performs the less than operation
+print(num1>=num2) # performs the graeter than or equal to operation
+print(num1<=num2) # performs the less than oequal to operation 
+print(num1==num2) # performs the equal to operation
+print(num1!=num2) # performs the not equal to operation
